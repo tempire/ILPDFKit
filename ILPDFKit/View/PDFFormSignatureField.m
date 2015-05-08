2d754ed805155c9bc29ff4e6ba50289fc22cc651
Glen Hinkle <glen@empireenterprises.com>
2015-05-07 20:26:07 -0600

    PDFWidgetColor for signaturefield

--- ILPDFKit/View/PDFFormSignatureField.m
+++ ILPDFKit/View/PDFFormSignatureField.m
@@ -31,7 +31,7 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.1];
+        self.backgroundColor = PDFWidgetColor;
 
         [self initializeControls];
     }
