a89d2c29789a6ba54bda21b1d6e43666e66cfb27
Glen Hinkle <glen@empireenterprises.com>
2015-10-26 00:37:01 -0600

    signatureImage property in PDFormSignatureField

--- ILPDFKit/View/PDFFormSignatureField.h
+++ ILPDFKit/View/PDFFormSignatureField.h
@@ -27,5 +27,5 @@
 /** The PDFFormSignatureField represents a view for a PDF signature field. Not currently implemented.
  */
 @interface PDFFormSignatureField : PDFWidgetAnnotationView
-@property (retain, nonatomic) UIImageView *imageView;
+@property (retain, nonatomic) UIImage *signatureImage;
 @end
